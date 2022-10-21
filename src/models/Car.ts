1aed150fb26bedc706d55f4edc3c78afe93a20b4
BrenoBTrindade <brenokaminski@hotmail.com>
2022-10-21 16:43:12 -0300

    feat: MongoModel and Model Car

---
+++ src/models/Car.ts
@@ -0,0 +1,20 @@
+import { string } from 'joi';
+import { model as mongoseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carMongooseSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  status: Boolean,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+export default class Car extends MongoModel<ICar> {
+  constructor(model = mongoseCreateModel('Car', carMongooseSchema)) {
+    super(model);
+  }
+}
