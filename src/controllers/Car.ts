b3cd072ebdf4a22dd0c4c9b73528ed83ed464b76
BrenoBTrindade <brenokaminski@hotmail.com>
2022-10-21 17:44:03 -0300

    feat: Car controller and Route

---
+++ src/controllers/Car.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { IService } from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  private _service:IService<ICar>;
+  constructor(service:IService<ICar>) {
+    this._service = service;
+  }
+
+  public async create(req: Request, res: Response<ICar>) {
+    const result = await this._service.create(req.body);
+    return res.status(200).json(result);
+  }
+
+  public async read(req: Request, res: Response<ICar[]>) {
+    const cars = await this._service.read();
+    return res.status(200).json(cars);
+  }
+
+  public async readOne(req:Request, res:Response<ICar>) {
+    const carById = await this._service.readOne(req.params.id);
+    return res.status(200).json(carById);
+  }
+
+  public async update(req:Request, res: Response<ICar>) {
+    const carUpdated = await this._service.update(req.params.id, req.body);
+    return res.status(200).json(carUpdated);
+  }
+
+  public async delete(req:Request, res: Response<ICar>) {
+    const carDeleted = await this._service.delete(req.params.id);
+    return res.status(200).json(carDeleted);
+  }
+}
---
+++ src/routes/car.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/Car';
+import CarService from '../services/Car';
+import Car from '../models/Car';
+
+const route = Router();
+
+const car = new Car();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+route.get('/cars', (req, res) => carController.read(req, res));
+
+export default route;
