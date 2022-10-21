5ec164cff9836164a63030c8d79060c7436c9988
BrenoBTrindade <brenokaminski@hotmail.com>
2022-10-21 17:55:21 -0300

    feat: route cars in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
-// commit inicial
+import 'express-async-errors';
+import errorHandler from './middlewares/errorMiddleware';
+import carRoutes from './routes/car.routes';
 
 const app = express();
+app.use(express.json());
+app.use(carRoutes);
+app.use(errorHandler);
 
 export default app;
